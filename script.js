4ddfa5b9e0232fb65e720976d350d18f33c678aa
vpsmolina <verasmolinap@mail.ru>
2020-11-10 22:05:47 +0300

    add asideList

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function formDate() {
     let day = cDate.getDate();
     if (day < 10) day = "0" + day;
     /*pngUrl = "https://ip-2-165.unn.ru:3389/cadi/CADIDATA/"+ year + "/" + month+ "/" + day+ "/";*/
-    pngUrl = "./cadi/CADIDATA/"+ year + "/0" + month+ "/0" + day+ "/";
+    pngUrl = "./cadi/CADIDATA/"+ year + "/" + month+ "/" + day+ "/";
     /*return document.getElementById("png").src = url;*/
-    let url = "img/"+ year + "/" + month+ "/" + day+ "/filenames.txt";
+    let url = "CADIDATA/"+ year + "/" + month+ "/" + day+ "/filenames.txt";
 
     fetch(url)
         .then(function(response) {
@@ -26,20 +26,34 @@ let printButton = document.formDate.print;
 printButton.addEventListener("click", formDate);
 
 let list = {};
+
 function done() {
     storedText.forEach((item, index) => {
         index = item[6] + item[7] + ":" + item[8] + item[9] + ":" + item[10] + item[11];
         list[index] = item;
         let url = pngUrl + item;
-        console.log(url);
+        /*console.log(url);*/
         /*document.getElementById("png").src = url;*/
         let result = index.link(url);
+/*
         document.getElementsByClassName('aside-list')[0].innerHTML += result;
+*/
+        let asideList = document.createElement('button');
+        asideList.className = 'aside-btn';
+        asideList.innerHTML = index;
+        asideList.onclick = function() {
+            document.getElementById("png").src = url;
+        };
+
+        aside.append(asideList);
     });
 }
 
-/*const latest = "https://ip-2-165.unn.ru:3389/cadi/latest.png";*/
-const latest = "./cadi/latest.png";
+/*document.getElementsByClassName('content-arrow').addEventListener("click", function () {
+    document.getElementById("png").src = url;
+});*/
+
+const latest = "./latest.png";
 
 const xhr = new XMLHttpRequest();
 
