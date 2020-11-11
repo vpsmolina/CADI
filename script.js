583585ca0c0944e6e62082b74da3273fbcd9ba8a
vpsmolina <verasmolinap@mail.ru>
2020-11-11 09:51:51 +0300

    check ""

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ function formDate() {
     if (month < 10) month = "0" + month;
     let day = cDate.getDate();
     if (day < 10) day = "0" + day;
-    /*pngUrl = "https://ip-2-165.unn.ru:3389/cadi/CADIDATA/"+ year + "/" + month+ "/" + day+ "/";*/
-    pngUrl = "./CADIDATA/"+ year + "/" + month+ "/" + day+ "/";
+    pngUrl = "https://ip-2-165.unn.ru:3389/cadi/CADIDATA/"+ year + "/" + month+ "/" + day+ "/";
+    /*pngUrl = "./CADIDATA/"+ year + "/" + month+ "/" + day+ "/";*/
     /*return document.getElementById("png").src = url;*/
     let url = "CADIDATA/"+ year + "/" + month+ "/" + day+ "/filenames.txt";
+
 
     fetch(url)
         .then(function(response) {
@@ -29,30 +30,32 @@ let list = {};
 
 function done() {
     storedText.forEach((item, index) => {
-        index = item[6] + item[7] + ":" + item[8] + item[9] + ":" + item[10] + item[11];
-        list[index] = item;
-        let url = pngUrl + item;
-        /*console.log(url);*/
-        /*document.getElementById("png").src = url;*/
-        let result = index.link(url);
-/*
-        document.getElementsByClassName('aside-list')[0].innerHTML += result;
-*/
-        let asideList = document.createElement('button');
-        asideList.className = 'aside-btn';
-        asideList.innerHTML = index;
-        asideList.onclick = function() {
-            document.getElementById("png").src = url;
-        };
+        if (item !== "") {
+            index = item[6] + item[7] + ":" + item[8] + item[9] + ":" + item[10] + item[11];
+            list[index] = item;
+            let url = pngUrl + item;
+            /*console.log(url);*/
+            /*document.getElementById("png").src = url;*/
+            let result = index.link(url);
+            /*
+                    document.getElementsByClassName('aside-list')[0].innerHTML += result;
+            */
+            let asideList = document.createElement('button');
+            asideList.className = 'aside-btn';
+            asideList.innerHTML = index;
+            asideList.onclick = function() {
+                document.getElementById("png").src = url;
+            };
 
-        aside.append(asideList);
+            aside.append(asideList);
+        }
     });
 }
 
 /*document.getElementsByClassName('content-arrow').addEventListener("click", function () {
     document.getElementById("png").src = url;
 });*/
-const latest = "./latest.png";
+/*const latest = "./latest.png";
 
 const xhr = new XMLHttpRequest();
 
@@ -60,4 +63,4 @@ xhr.open('GET', latest);
 
 xhr.send();
 
-document.getElementById("png").src = latest;
+document.getElementById("png").src = latest;*/
