363e27e5452a92b5804698dc3ecd48f7a26d251a
vpsmolina <verasmolinap@mail.ru>
2020-12-02 19:45:27 +0300

    error Filenames

--- script.js
+++ script.js
@@ -6,18 +6,25 @@ let prev;
 let asideList;
 let asideP = document.getElementById("aside");
 
-const latest = "./latest.png";
+/*const latest = "./latest.png";
 const xhr = new XMLHttpRequest();
 xhr.open('GET', latest);
 xhr.send();
-document.getElementById("png").src = latest;
+document.getElementById("png").src = latest;*/
 
 document.formDate.print.addEventListener("click", checkList);
 
 document.getElementsByClassName('prev')[0].addEventListener("click", function () {
     prev = document.getElementById("png").src;
+    /*console.log(asideList);*/
     listAside.forEach((element, index, arr) => {
-        if (element == prev.slice(-16) && index !== 0) document.getElementById("png").src = pngUrl + arr[index-1];
+        if (element == prev.slice(-16) && index !== 0) {
+            document.getElementById("png").src = pngUrl + arr[index-1];
+            /*document.getElementsByClassName('aside-btn').innerHTML;*/
+            let key = Object.keys(list)[Object.values(list).indexOf(element)];
+            /*style.backgroundColor = "#bc1423";*/
+
+        }
     });
 });
 
@@ -41,8 +48,8 @@ function formDate() {
     const month = (cDate.getMonth() + 1) < 10 ? `0${cDate.getMonth() + 1}` : `${cDate.getMonth() + 1}`;
     const day = (cDate.getDate()) < 10 ? `0${cDate.getDate()}` : `${cDate.getDate()}`;
     const baseUrl = `CADIDATA/${year}/${month}/${day}/`;
-    /*pngUrl = "https://ip-2-165.unn.ru:3389/cadi/CADIDATA/"+ year + "/" + month+ "/" + day+ "/";*/
-    pngUrl = `./${baseUrl}`;
+    pngUrl = `https://ip-2-165.unn.ru:3389/cadi/CADIDATA/${year}/${month}/${day}/`;
+    /*pngUrl = `./${baseUrl}`;*/
     let url = `${baseUrl}filenames.txt`;
 
 
@@ -50,12 +57,18 @@ function formDate() {
         .then(function(response) {
             if(response.ok) {
                 response.text().then(function(text) {
-                    /*storedText = text.split('\r\n');*/
-                    storedText = text.split('\n');
+                    storedText = text.split('\r\n');
+                    /*storedText = text.split('\n');*/
 
                     done();
                 });
-            } else console.log(`Network request for filenames.txt failed with response ${response.status}: ${response.statusText}`);
+            } else {
+                console.log(`Network request for filenames.txt failed with response ${response.status}: ${response.statusText}`);
+                let errFilenames = document.createElement("div");
+                errFilenames.className = "err-filenames";
+                errFilenames.innerHTML = `${day}.${month}.${year} данных не найдено, выберите другой день.`;
+                asideP.append(errFilenames);
+            }
         });
 }
 
@@ -76,6 +89,7 @@ function done() {
                 document.getElementById("png").src = url;
                 prev = event.currentTarget.previousElementSibling.innerHTML;
             }
+            
             asideP.append(asideList);
         }
     });
